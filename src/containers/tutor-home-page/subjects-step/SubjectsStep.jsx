215abf0184f30b130f485d1b1dfa304366372b78
ImDarkly <svit.darkly@gmail.com>
2026-03-27 22:51:39 +0200

    feat(subjects-step): add image, category/subject selectors, chips list, and add button

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,12 +1,131 @@
 import Box from '@mui/material/Box'
 
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
+import img from '~/assets/img/tutor-home-page/become-tutor/study-category.svg'
+import { Button, FormHelperText, Typography } from '@mui/material'
+import { useTranslation } from 'react-i18next'
+import AsyncAutocomplete from '~/components/async-autocomlete/AsyncAutocomplete'
+import { useMemo, useState } from 'react'
+import { categoryService } from '~/services/category-service'
+import { useStepContext } from '~/context/step-context'
+import AppChipList from '~/components/app-chips-list/AppChipList'
+import useBreakpoints from '~/hooks/use-breakpoints'
+import { subjectService } from '~/services/subject-service'
+import { mockGetCategories } from './constants'
+import { mockGetSubjects } from './constants'
+
+const USE_MOCK = true
 
 const SubjectsStep = ({ btnsBox }) => {
+  const { t } = useTranslation()
+  const { isLaptopAndAbove, isMobile } = useBreakpoints()
+  const [subjectError, setSubjectError] = useState()
+  const [subjects, setSubjects] = useState({
+    category: null,
+    subject: null
+  })
+  const { stepData, handleStepData } = useStepContext()
+  const subjectNames = stepData.subjects.map((subject) => subject.name)
+
+  const categoryServiceFn = USE_MOCK
+    ? mockGetCategories
+    : categoryService.getCategoriesNames
+
+  const categoryId = subjects.category?._id
+  const subjectsServiceFn = useMemo(
+    () =>
+      USE_MOCK
+        ? mockGetSubjects(categoryId)
+        : subjectService.getSubjectsNames(categoryId),
+    [categoryId]
+  )
+
+  const onChangeCategory = (_, value) => {
+    setSubjects((prev) =>
+      prev.category?._id !== value?._id
+        ? {
+            category: value,
+            subject: null
+          }
+        : prev
+    )
+  }
+
+  const onChangeSubject = (_, value) => {
+    setSubjects((prev) => ({ category: prev.category, subject: value }))
+  }
+
+  const addSubject = () => {
+    if (!subjects.subject && !subjects.category) {
+      setSubjectError(t('becomeTutor.categories.emptyFields'))
+      return
+    }
+
+    const isSameSubject = stepData.subjects.some(
+      (s) => s._id === subjects.subject?._id
+    )
+
+    if (isSameSubject) {
+      setSubjectError(t('becomeTutor.categories.sameSubject'))
+      return
+    }
+
+    handleStepData('subjects', [...stepData.subjects, subjects.subject])
+    setSubjectError(null)
+    setSubjects({
+      category: null,
+      subject: null
+    })
+  }
+
+  const removeSubject = (name) => {
+    const updated = stepData.subjects.filter((subject) => subject.name !== name)
+    handleStepData('subjects', updated)
+  }
+
+  const imageContainer = (
+    <Box sx={styles.imgContainer}>
+      <Box component='img' src={img} sx={styles.img} />
+    </Box>
+  )
+
   return (
     <Box sx={styles.container}>
-      <Box sx={styles.rigthBox}>
-        Subjects step
+      {isLaptopAndAbove && imageContainer}
+      <Box sx={styles.rightBox}>
+        {isMobile && imageContainer}
+        <Box sx={styles.contentBox}>
+          <Typography>{t('becomeTutor.categories.title')}</Typography>
+          <AsyncAutocomplete
+            labelField='name'
+            onChange={onChangeCategory}
+            service={categoryServiceFn}
+            textFieldProps={{
+              label: t('becomeTutor.categories.mainSubjectsLabel')
+            }}
+            value={subjects.category}
+          />
+          <AsyncAutocomplete
+            disabled={!subjects.category}
+            fetchCondition={!!subjects.category}
+            labelField='name'
+            onChange={onChangeSubject}
+            service={subjectsServiceFn}
+            textFieldProps={{
+              label: t('becomeTutor.categories.subjectLabel')
+            }}
+            value={subjects.subject}
+          />
+          <Button fullWidth onClick={addSubject} variant='tonal'>
+            {t('becomeTutor.categories.btnText')}
+          </Button>
+          <AppChipList
+            defaultQuantity={4}
+            handleChipDelete={removeSubject}
+            items={subjectNames}
+          />
+          <FormHelperText error={subjectError}>{subjectError}</FormHelperText>
+        </Box>
         {btnsBox}
       </Box>
     </Box>
--- src/containers/tutor-home-page/subjects-step/constants.js
+++ src/containers/tutor-home-page/subjects-step/constants.js
@@ -1,33 +1,52 @@
-export const categoriesMock = [
-  { name: 'Languages' },
-  { name: 'Mathematics' },
-  { name: 'Computer science' },
-  { name: 'Music' },
-  { name: 'History' }
-]
+export const mockGetCategories = () =>
+  Promise.resolve({
+    data: [
+      { _id: '1', name: 'Mathematics' },
+      { _id: '2', name: 'Physics' },
+      { _id: '3', name: 'Computer Science' },
+      { _id: '4', name: 'Chemistry' },
+      { _id: '5', name: 'Biology' }
+    ]
+  })
 
-export const languagesMock = [
-  { name: 'Chinese' },
-  { name: 'Czech' },
-  { name: 'Danish' },
-  { name: 'Dutch' },
-  { name: 'English' },
-  { name: 'Estonian' },
-  { name: 'Finnish' },
-  { name: 'French' },
-  { name: 'German' },
-  { name: 'Hungarian' },
-  { name: 'Icelandic' },
-  { name: 'Italian' },
-  { name: 'Japanese' },
-  { name: 'Korean' },
-  { name: 'Norwegian' },
-  { name: 'Polish' },
-  { name: 'Portuguese (Brazil)' },
-  { name: 'Portuguese (Portugal)' },
-  { name: 'Romanian' },
-  { name: 'Slovak' },
-  { name: 'Spanish' },
-  { name: 'Swedish' },
-  { name: 'Ukrainian' }
-]
+export const mockGetSubjects = (categoryId) => () =>
+  Promise.resolve({
+    data:
+      categoryId === '1'
+        ? [
+            { _id: '11', name: 'Algebra' },
+            { _id: '12', name: 'Geometry' },
+            { _id: '13', name: 'Calculus' },
+            { _id: '14', name: 'Probability Theory' },
+            { _id: '15', name: 'Linear Algebra' }
+          ]
+        : categoryId === '2'
+          ? [
+              { _id: '21', name: 'Mechanics' },
+              { _id: '22', name: 'Optics' },
+              { _id: '23', name: 'Thermodynamics' },
+              { _id: '24', name: 'Electromagnetism' },
+              { _id: '25', name: 'Quantum Physics' }
+            ]
+          : categoryId === '3'
+            ? [
+                { _id: '31', name: 'Algorithms' },
+                { _id: '32', name: 'Data Structures' },
+                { _id: '33', name: 'Operating Systems' },
+                { _id: '34', name: 'Databases' },
+                { _id: '35', name: 'Computer Networks' }
+              ]
+            : categoryId === '4'
+              ? [
+                  { _id: '41', name: 'Organic Chemistry' },
+                  { _id: '42', name: 'Inorganic Chemistry' },
+                  { _id: '43', name: 'Physical Chemistry' },
+                  { _id: '44', name: 'Analytical Chemistry' }
+                ]
+              : [
+                  { _id: '51', name: 'Cell Biology' },
+                  { _id: '52', name: 'Genetics' },
+                  { _id: '53', name: 'Ecology' },
+                  { _id: '54', name: 'Human Anatomy' }
+                ]
+  })
