24ae644929d6e64f978f82d59b1f2483926d3f5b
ImDarkly <svit.darkly@gmail.com>
2026-04-02 18:52:06 +0300

    feat(stepper): confirm before closing first-login stepper modal

--- src/pages/student-home/StudentHome.tsx
+++ src/pages/student-home/StudentHome.tsx
@@ -8,15 +8,29 @@ import FindBlock from '~/components/find-block/FindBlock'
 import Faq from '~/containers/student-home-page/faq/Faq'
 
 import { translationKey } from '~/components/find-block/find-tutor-constants'
+import { useTranslation } from 'react-i18next'
+import useConfirm from '~/hooks/use-confirm'
 
 const StudentHome = () => {
-  const { openModal } = useModalContext()
+  const { t } = useTranslation()
+  const { openModal, closeModal } = useModalContext()
   const { isFirstLogin, userRole } = useAppSelector((state) => state.appMain)
+  const { checkConfirmation } = useConfirm()
 
   useEffect(() => {
     if (isFirstLogin) {
       openModal({
         component: <UserStepsWrapper userRole={userRole} />,
+        onCloseRequest: async () => {
+          const confirmed = checkConfirmation({
+            title: 'titles.confirmTitle',
+            message: 'questions.unsavedChanges',
+            confirmButton: t('common.discard'),
+            cancelButton: t('common.cancel'),
+            check: true
+          })
+          if (await confirmed) closeModal()
+        },
         paperProps: {
           sx: {
             maxHeight: { sm: '652px' },
