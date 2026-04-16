2cf4e357afd8edf62c076eda904ed92495f6a630
imdarkly <svit.darkly@gmail.com>
2026-04-16 17:54:31 +0300

    feat(tutor-language-step): implement language selection UI with i18n support

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -1,16 +1,53 @@
 import Box from '@mui/material/Box'
+import { Typography, TextField, Autocomplete } from '@mui/material'
 
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
 import img from '~/assets/img/tutor-home-page/become-tutor/languages.svg'
+import useBreakpoints from '~/hooks/use-breakpoints'
+import { useTranslation } from 'react-i18next'
+import { useState } from 'react'
+
+const languages = [
+  { label: 'English', code: 'en' },
+  { label: 'Ukrainian', code: 'uk' },
+  { label: 'Polish', code: 'pl' }
+]
 
 const LanguageStep = ({ btnsBox }) => {
+  const { t } = useTranslation()
+  const { isLaptopAndAbove, isMobile } = useBreakpoints()
+  const [value, setValue] = useState(null)
+
+  const imageContainer = (
+    <Box sx={styles.imgContainer}>
+      <Box component='img' src={img} sx={styles.img} />
+    </Box>
+  )
+
   return (
     <Box sx={styles.container}>
-      <Box sx={styles.imgContainer}>
-        <Box component='img' src={img} sx={styles.img} />
-      </Box>
+      {isLaptopAndAbove && imageContainer}
       <Box sx={styles.rigthBox}>
-        <Box>Language here</Box>
+        {isMobile && imageContainer}
+        <Box sx={styles.contentBox}>
+          <Typography>{t('becomeTutor.languages.title')}</Typography>
+
+          <Autocomplete
+            getOptionLabel={(option) => option.label}
+            onChange={(e, newValue) => setValue(newValue)}
+            options={languages}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                fullWidth
+                placeholder={t('becomeTutor.languages.autocompleteLabel')}
+                variant='outlined'
+              />
+            )}
+            sx={{ mt: 2 }}
+            value={value}
+          />
+        </Box>
         {btnsBox}
       </Box>
     </Box>
--- src/containers/tutor-home-page/language-step/LanguageStep.styles.js
+++ src/containers/tutor-home-page/language-step/LanguageStep.styles.js
@@ -26,5 +26,11 @@ export const styles = {
     justifyContent: 'space-between',
     m: { md: 0, xs: '0 auto' },
     pt: 0
+  },
+  contentBox: {
+    mb: { xs: '30px', sm: '0' },
+    display: 'flex',
+    flexDirection: 'column',
+    gap: '20px'
   }
 }
