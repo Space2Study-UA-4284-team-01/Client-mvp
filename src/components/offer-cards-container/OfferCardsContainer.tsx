2e28d9e11f53e04398d8cdad157281acc102dd24
Anna <annayattsenko@gmail.com>
2026-04-17 13:27:43 +0300

    make offer cards container controlled via props

--- src/components/offer-cards-container/OfferCardsContainer.tsx
+++ src/components/offer-cards-container/OfferCardsContainer.tsx
@@ -1,49 +1,30 @@
-import { FC, ReactNode, useState } from 'react'
-import { useTranslation } from 'react-i18next'
+import { FC, ReactNode } from 'react'
 import Box from '@mui/material/Box'
-
-import AppContentSwitcher from '~/components/app-content-switcher/AppContentSwitcher'
-import { TypographyVariantEnum } from '~/types'
 
 interface OfferCardsContainerProps {
   cards: ReactNode[]
+  isList: boolean
 }
 
-const OfferCardsContainer: FC<OfferCardsContainerProps> = ({ cards }) => {
-  const [isList, setIsList] = useState(false)
-  const { t } = useTranslation()
-
-  const onChange = () => setIsList((prev) => !prev)
-
-  const switchOptions = {
-    left: { text: t('common.grid') },
-    right: { text: t('common.list') }
-  }
-
+const OfferCardsContainer: FC<OfferCardsContainerProps> = ({
+  cards,
+  isList
+}) => {
   return (
-    <Box>
-      <AppContentSwitcher
-        active={isList}
-        onChange={onChange}
-        switchOptions={switchOptions}
-        typographyVariant={TypographyVariantEnum.Body2}
-      />
-
-      <Box
-        sx={{
-          display: 'grid',
-          gridTemplateColumns: isList
-            ? '1fr'
-            : {
-                xs: 'repeat(1, 1fr)',
-                sm: 'repeat(2, 1fr)',
-                md: 'repeat(3, 1fr)'
-              },
-          gap: '24px'
-        }}
-      >
-        {cards}
-      </Box>
+    <Box
+      sx={{
+        display: 'grid',
+        gridTemplateColumns: isList
+          ? '1fr'
+          : {
+              xs: 'repeat(1, 1fr)',
+              sm: 'repeat(2, 1fr)',
+              md: 'repeat(3, 1fr)'
+            },
+        gap: '24px'
+      }}
+    >
+      {cards}
     </Box>
   )
 }
