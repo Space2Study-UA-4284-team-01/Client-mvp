6dbefe3f503fa4a1686eb696acbbca9d9c200e8c
ImDarkly <svit.darkly@gmail.com>
2026-04-24 16:48:04 +0300

    refactor(find-offers): improve formatting and extend imports

--- src/pages/find-offers/FindOffers.tsx
+++ src/pages/find-offers/FindOffers.tsx
@@ -17,12 +17,24 @@ import DirectionLink from '~/components/direction-link/DirectionLink'
 import AppToolbar from '~/components/app-toolbar/AppToolbar'
 import SortMenu from '~/components/sort-menu/SortMenu'
 
-import { SizeEnum, ButtonVariantEnum, CategoryInterface, ItemsWithCount, Sort, SortEnum } from '~/types'
+import {
+  SizeEnum,
+  ButtonVariantEnum,
+  CategoryInterface,
+  ItemsWithCount,
+  Sort,
+  SortEnum
+} from '~/types'
 import { authRoutes } from '~/router/constants/authRoutes'
 import { defaultResponses } from '~/constants'
 import { getOpositeRole } from '~/utils/helper-functions'
 import { styles } from '~/pages/find-offers/FindOffers.styles'
 import heroImage from '~/assets/img/find-offer/subject_icon.png'
+import { useAppSelector } from '~/hooks/use-redux'
+import { categoryService } from '~/services/category-service'
+import useAxios from '~/hooks/use-axios'
+import CategoryCard from '~/components/category-card/CategoryCard'
+import AppButton from '~/components/app-button/AppButton'
 
 const FindOffers = () => {
   const { t } = useTranslation()
@@ -35,8 +47,7 @@ const FindOffers = () => {
   const oppositeRole = getOpositeRole(userRole)
 
   const { response } = useAxios<ItemsWithCount<CategoryInterface>>({
-    service: () =>
-      categoryService.getCategories({ limit: 9, skip: 0, sort }),
+    service: () => categoryService.getCategories({ limit: 9, skip: 0, sort }),
     defaultResponse: defaultResponses.itemsWithCount
   })
 
@@ -68,23 +79,33 @@ const FindOffers = () => {
       <Box sx={styles.heroBox}>
         <Box sx={styles.heroContent}>
           <Typography mb='8px' variant='h5'>
-            {t('findOffers.hero.title', { defaultValue: 'Tutors for private lessons' })}
+            {t('findOffers.hero.title', {
+              defaultValue: 'Tutors for private lessons'
+            })}
           </Typography>
           <Typography mb='16px' variant='body2'>
             {t('findOffers.hero.description', {
-              defaultValue: 'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
+              defaultValue:
+                'It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout.'
             })}
           </Typography>
           <Button sx={{ alignSelf: 'flex-start' }} variant='contained'>
             {t('findOffers.hero.button', { defaultValue: 'Create request' })}
           </Button>
         </Box>
-        <Box alt='hero illustration' component='img' src={heroImage} sx={styles.heroImage} />
+        <Box
+          alt='hero illustration'
+          component='img'
+          src={heroImage}
+          sx={styles.heroImage}
+        />
       </Box>
 
       <Box sx={styles.contentWrapper}>
         <TitleWithDescription
-          description={t('findOffers.description', { defaultValue: 'Discover offers in your area of interest' })}
+          description={t('findOffers.description', {
+            defaultValue: 'Discover offers in your area of interest'
+          })}
           style={styles.titleWithDescription}
           title={t('findOffers.title', { defaultValue: 'Explore Offers' })}
         />
@@ -93,7 +114,9 @@ const FindOffers = () => {
           <DirectionLink
             before={<ArrowBackIcon fontSize={SizeEnum.Small} />}
             linkTo={authRoutes.categories.path}
-            title={t('subjectsPage.subjects.backToAllCategories', { defaultValue: 'Back to all categories' })}
+            title={t('subjectsPage.subjects.backToAllCategories', {
+              defaultValue: 'Back to all categories'
+            })}
           />
         </Box>
 
@@ -101,7 +124,10 @@ const FindOffers = () => {
           <TextField
             label={t('findOffers.category', { defaultValue: 'Category' })}
             onChange={(e) => setCategory(e.target.value)}
-            select size='small' sx={styles.categoryInput} value={category}
+            select
+            size='small'
+            sx={styles.categoryInput}
+            value={category}
           >
             <MenuItem value='1'>Languages</MenuItem>
           </TextField>
@@ -109,7 +135,10 @@ const FindOffers = () => {
           <TextField
             label={t('findOffers.subject', { defaultValue: 'Subject' })}
             onChange={(e) => setSubject(e.target.value)}
-            select size='small' sx={styles.categoryInput} value={subject}
+            select
+            size='small'
+            sx={styles.categoryInput}
+            value={subject}
           >
             <MenuItem value='1'>English</MenuItem>
           </TextField>
@@ -117,15 +146,25 @@ const FindOffers = () => {
           <TextField
             InputProps={{
               startAdornment: (
-                <InputAdornment position='start'><SearchIcon /></InputAdornment>
+                <InputAdornment position='start'>
+                  <SearchIcon />
+                </InputAdornment>
               )
             }}
             onChange={(e) => setSearch(e.target.value)}
-            placeholder={t('findOffers.searchPlaceholder', { defaultValue: 'Search by tutor name' })}
-            size='small' sx={styles.searchInput} value={search}
+            placeholder={t('findOffers.searchPlaceholder', {
+              defaultValue: 'Search by tutor name'
+            })}
+            size='small'
+            sx={styles.searchInput}
+            value={search}
           />
 
-          <Button onClick={handleSearch} sx={styles.searchButton} variant='contained'>
+          <Button
+            onClick={handleSearch}
+            sx={styles.searchButton}
+            variant='contained'
+          >
             {t('findOffers.search', { defaultValue: 'Search' })}
           </Button>
         </AppToolbar>
